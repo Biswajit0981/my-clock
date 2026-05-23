a9c36b344653e57d8289d677b45e9599b33885c2
Biswajit0981 <codingpro0981@gmail.com>
2026-05-23 12:04:05 +0530

    Add buttons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,24 @@
+import {useState} from "react";
+import {LuClock5} from "react-icons/lu";
+import {IoIosColorPalette} from "react-icons/io";
+import {BsFullscreen} from "react-icons/bs";
+
 const Footer = () => {
+    const [type, setType] = useState<boolean>(false);
+
     return (
-        <footer className="clock_footer">Footer</footer>
+        <div className="clock_menu">
+            <button className="hours_btn" onClick={() => setType((prev) => !prev)}>{type ? "24h" : "12h"}</button>
+            <button className="style_btn">
+                <IoIosColorPalette className="text-white text-4xl"/>
+            </button>
+            <button className="style_btn ">
+                <LuClock5 className="text-white text-4xl"/>
+            </button>
+            <button className="style_btn ">
+                <BsFullscreen className="text-white text-4xl"/>
+            </button>
+        </div>
     )
 }
 
