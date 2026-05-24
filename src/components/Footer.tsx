72f26b5a5924fa31695fb9e0024b6fb922a821a5
Biswajit0981 <codingpro0981@gmail.com>
2026-05-24 12:04:08 +0530

    add color state on btn

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,29 +5,34 @@ import {FaStopwatch20} from "react-icons/fa6";
 import {useClockContext} from "../context/ClockProvider.ts";
 import TimerPop from "./TimerPop.tsx";
 import {useState} from "react";
+import ColorPop from "./ColorPop.tsx";
 
 const Footer = () => {
     const  {state, dispatch} = useClockContext();
 
-     const [toggle, setToggle] = useState(false);
+     const [timerToggle, setTimerToggle] = useState(false);
+     const [colorToggle, setColorToggle] = useState(false);
 
     return (
         <div className="clock_menu">
             {
-                toggle && <TimerPop/>
+                colorToggle && <ColorPop/>
             }
-            <button className="hours_btn" onClick={() => dispatch({type: "hour"})}>{state.hour ? "24h" : "12h"}</button>
-            <button className="style_btn">
-                <IoIosColorPalette className="text-white text-4xl"/>
+            {
+                timerToggle && <TimerPop/>
+            }
+            <button className="hours_btn" onClick={() => dispatch({type: "hour"})} type="button">{state.hour ? "24h" : "12h"}</button>
+            <button aria-label="Open color palette" className="style_btn" onClick={() => setColorToggle((prev) => !prev)} type="button">
+                <IoIosColorPalette className="style_icon"/>
             </button>
-            <button className="style_btn " onClick={() => setToggle((prev) => !prev)}>
-                <LuClock5 className="text-white text-4xl"/>
+            <button aria-label="Open timer settings" className="style_btn" onClick={() => setTimerToggle((prev) => !prev)} type="button">
+                <LuClock5 className="style_icon"/>
             </button>
-            <button className="style_btn" onClick={() => dispatch({type: "stopwatch"})}>
-                <FaStopwatch20 className="text-white text-4xl"/>
+            <button aria-label="Toggle stopwatch" className="style_btn" onClick={() => dispatch({type: "stopwatch"})} type="button">
+                <FaStopwatch20 className="style_icon"/>
             </button>
-            <button className="style_btn " onClick={() => document.documentElement.requestFullscreen()}>
-                <BsFullscreen className="text-white text-4xl"/>
+            <button aria-label="Enter fullscreen" className="style_btn" onClick={() => document.documentElement.requestFullscreen()} type="button">
+                <BsFullscreen className="style_icon"/>
             </button>
         </div>
     )
