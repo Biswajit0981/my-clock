6432a774bd36221e4f0cc4f72335b1ca9c9542d5
Biswajit0981 <codingpro0981@gmail.com>
2026-05-24 07:26:39 +0530

    remove props dealing via context API

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,23 @@
-import {useState} from "react";
+
 import {LuClock5} from "react-icons/lu";
 import {IoIosColorPalette} from "react-icons/io";
 import {BsFullscreen} from "react-icons/bs";
-
+import { FaStopwatch20 } from "react-icons/fa6";
+import {useClockContext} from "../context/ClockProvider.ts";
 const Footer = () => {
-    const [type, setType] = useState<boolean>(false);
+    const {state, dispatch} = useClockContext();
 
     return (
         <div className="clock_menu">
-            <button className="hours_btn" onClick={() => setType((prev) => !prev)}>{type ? "24h" : "12h"}</button>
+            <button className="hours_btn" onClick={() => dispatch({type: "hour"})}>{state.hour ? "24h" : "12h"}</button>
             <button className="style_btn">
                 <IoIosColorPalette className="text-white text-4xl"/>
             </button>
             <button className="style_btn ">
                 <LuClock5 className="text-white text-4xl"/>
+            </button>
+            <button className="style_btn ">
+                <FaStopwatch20 className="text-white text-4xl"/>
             </button>
             <button className="style_btn ">
                 <BsFullscreen className="text-white text-4xl"/>
